ead105063a854e1720240846a47887375573b42f
Dayanthan <daya.naukri@gmail.com>
2012-05-23 15:11:09 +0530

    forgot and resetpassword added

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
 
   validates :password, :presence => true,
                        :confirmation => true,
-                       :length => { :within => 6..40 
+                       :length => { :within => 6..40 }
 
   #delegate :url_helpers, to: 'Rails.application.routes' 
 
